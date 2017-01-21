7705022adc0a03bfe07db2bad81c12801ffdf3c8
niravmandani <niravmandnai@example.com>
2017-01-21 17:55:18 +0530

    After ItemSelection update

--- app/src/main/java/com/example/av004/retrofitexample/list/UsersAdapter.java
+++ app/src/main/java/com/example/av004/retrofitexample/list/UsersAdapter.java
@@ -2,11 +2,14 @@ package com.example.av004.retrofitexample.list;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.av004.retrofitexample.R;
@@ -18,7 +21,6 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnItemClick;
 
-
 /**
  * Created by AV004 on 1/3/2017.
  */
@@ -26,11 +28,12 @@ import butterknife.OnItemClick;
 public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.MyViewHolder> {
     private List<User> usersList;
     public int position;
+    private SharedPreferences mPref;
+    private SharedPreferences.Editor mEditor;
+    RelativeLayout list_row;
     OnUserSelectListener onUserSelectListener;
     OnUserDeleteListener onUserDeleteListener;
     Context context;
-
-
 
     public interface OnUserDeleteListener {
 
@@ -49,16 +52,20 @@ public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.MyViewHolder
         TextView textviewemail;
         @Bind(R.id.txtcity)
         TextView textviewcity;
+        RelativeLayout list_row;
 
         public MyViewHolder(View view) {
             super(view);
             ButterKnife.bind(this, view);
+            list_row = (RelativeLayout) view.findViewById(R.id.list_row);
         }
     }
 
     public UsersAdapter(Context context, List<User> usersList) {
         this.usersList = usersList;
         this.context = context;
+        mPref = context.getSharedPreferences("user", Context.MODE_PRIVATE);
+        mEditor = mPref.edit();
     }
 
     @Override
@@ -77,12 +84,16 @@ public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.MyViewHolder
         holder.textviewname.setText(user.getName());
         holder.textviewemail.setText(user.getEmail());
         holder.textviewcity.setText(user.getAddress().getCity());
-
+        if (usersList.get(position).isSelected()) {
+            holder.list_row.setBackgroundColor(Color.parseColor("#7B8BA0"));
+        } else {
+            holder.list_row.setBackgroundColor(Color.TRANSPARENT);
+        }
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             @OnItemClick()
             public void onClick(View v) {
-
+                setSelected(position);
                 onUserSelectListener = (OnUserSelectListener) context;
                 onUserSelectListener.setOnUserSelect(position);
 
@@ -95,7 +106,20 @@ public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.MyViewHolder
                 return false;
             }
         });
+    }
 
+    public void setSelected(int pos) {
+        try {
+            if (usersList.size() > 1) {
+                usersList.get(mPref.getInt("position", 0)).setSelected(false);
+                mEditor.putInt("position", pos);
+                mEditor.commit();
+            }
+            usersList.get(pos).setSelected(true);
+            notifyDataSetChanged();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
